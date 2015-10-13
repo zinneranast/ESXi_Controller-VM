d428b6888533465b4f5f9628bebc951f723bd4be
zinneranast <zinneranast@mail.ru>
2015-10-14 07:24:57 +0800

    add forms for watching port groups and virtual machines

--- django-projects/CS/urls.py
+++ django-projects/CS/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'service_list/', views.service_list, name='service_list'),
+    url(r'services/', views.services, name='services'),
     url(r'connect/', views.connect, name='connect'),
     url(r'login/', views.login, name='login'),
+    url(r'control/', views.control, name='control'),
+    url(r'portgroups/', views.portgroups, name='portgroups'),
+    url(r'virtmachines/', views.virtmachines, name='virtmachines'),
 ]
--- django-projects/CS/views.py
+++ django-projects/CS/views.py
@@ -4,24 +4,38 @@ from django.http import HttpResponse
 import subprocess
 import os
 
-def service_list(request):
+def services(request):
     services = Service.objects.order_by('ServiceName')
-    return render(request, 'CS/service_list.html', {'name': 'Alex', 'surname': 'Ivanov', 'email': 'ivanov.alex@gmail.com', 'ip_address': '192.168.1.201', 'services': services})
-
-from django.core.exceptions import ValidationError
+    return render(request, 'CS/services.html', {'name': 'Alex', 'surname': 'Ivanov', 'email': 'ivanov.alex@gmail.com', 'ip_address': '192.168.1.201', 'services': services})
 
 def connect(request):
-    isClient = Client.objects.filter(ClientEmail = request.GET['email'])
-    if not isClient.count:
-        Client.objects.create(ClientName = request.GET['name'], ClientSurname = request.GET['surname'], ClientEmail = request.GET['email'])
+    isClientExists = Client.objects.filter(ClientEmail = request.GET['email'])
+    if not isClientExists.count():
+        client = Client(ClientName = request.GET['name'], ClientSurname = request.GET['surname'], ClientEmail = request.GET['email'])
+        client.save()
+
     c = Client.objects.get(ClientEmail = request.GET['email'])
     s = Service.objects.get(ServiceName = request.GET['service'])
+
     ClientService.objects.create(ClientId = c.ClientId, ServiceId = s.ServiceId, IpAddress = request.GET['ip_address'])
     #os.system('sudo ./VCLIConfigurator.sh userPortGroup VM1 %s %s' % (request.GET['service'], request.GET['ip_address']))
-    return HttpResponse('win!')
+    return HttpResponse('The service was connected succesfully.')
 
 def login(request):
     email = request.GET.get('email')
     password = request.GET.get('password')
     return render(request, 'CS/login.html', {'email': email, 'password': password})
+
+def control(request):
+    return render(request, 'CS/control.html')
+
+def portgroups(request):
+    getPortGroups = subprocess.Popen("esxcli -c sessionConfig.cfg network vswitch standard portgroup list", shell=True, stdout=subprocess.PIPE)
+    portGroups = getPortGroups.stdout.readlines()
+    return render(request, 'CS/portgroups.html', {'portGroups': portGroups})
+
+def virtmachines(request):
+    getVirtMachines = subprocess.Popen("vmware-cmd --config sessionConfig.cfg -l", shell=True, stdout=subprocess.PIPE)
+    virtMachines = getVirtMachines.stdout.readlines()
+    return render(request, 'CS/virtmachines.html', {'virtMachines': virtMachines})
 
